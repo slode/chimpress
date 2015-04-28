30d7da0ce57d9a546b42b233669d9a78288f2d93
Christian Stigen <Christian.Stigen@Emerson.com>
2015-04-28 16:13:56 +0200

    Adds autotools.
    I rock.

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 file="$1" || "datafiles/datafile.txt"
 
 cp $file tmp0
-time ./bin/rle_enc < tmp0 > tmp1
-time ./bin/rle_dec < tmp1 > tmp2
+time ./src/rle_enc < tmp0 > tmp1
+time ./src/rle_dec < tmp1 > tmp2
 diff $file tmp2
 ls -Flah | grep tmp
 rm tmp0 tmp1 tmp2
 
 cp $file tmp0
-time ./bin/lzw_enc < tmp0 > tmp1
-time ./bin/lzw_dec < tmp1 > tmp2
+time ./src/lzw_enc < tmp0 > tmp1
+time ./src/lzw_dec < tmp1 > tmp2
 diff $file tmp2
 ls -Flah | grep tmp
 #xxd tmp0
@@ -18,8 +18,8 @@ ls -Flah | grep tmp
 rm tmp0 tmp1 tmp2
 
 cp $file tmp0
-time ./bin/rle_enc < tmp0 | ./bin/lzw_enc > tmp1
-time ./bin/lzw_dec < tmp1 | ./bin/rle_dec > tmp2
+time ./src/rle_enc < tmp0 | ./src/lzw_enc > tmp1
+time ./src/lzw_dec < tmp1 | ./src/rle_dec > tmp2
 diff $file tmp2
 ls -Flah | grep tmp
 #xxd tmp0
